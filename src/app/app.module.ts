008777cf2ccc4636d7497970663ce78727bff528
Jeremy Uriz <jjuriz@gmail.com>
2023-03-04 13:49:54 -0500

    Init Commit - Initialize Project

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { SignupComponent } from './signup/signup/signup.component';
+import { SigninComponent } from './signin/signin/signin.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SignupComponent,
+    SigninComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
